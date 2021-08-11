e9aa83d92692beb7a12077ee5c6d581d2cebe2b7
Aman Seth <aman.seth@devfacto.com>
2021-08-11 14:25:14 -0600

    set up Linting

--- app/ClientApp/src/components/Counter.tsx
+++ app/ClientApp/src/components/Counter.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 export const Counter = () => {
   const [currentCount, setCurrentCount] = React.useState(0);
+  // var [currentCount, setCurrentCount] = React.useState(0); // change to var
+  // var currentCount = 1; // redeclaring a variable. that should make it mad!
   return (
     <div>
       <h1>Counter</h1>
