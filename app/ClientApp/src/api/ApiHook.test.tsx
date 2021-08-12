8bbc0634f5f63b11b580762acccd25b6d2110556
Aman Seth <aman.seth@devfacto.com>
2021-08-12 15:31:57 -0600

    front end testing with jest, add Api Hook testing

---
+++ app/ClientApp/src/api/ApiHook.test.tsx
@@ -0,0 +1,18 @@
+import useApi from './ApiHook';
+import axios from 'axios';
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+it('should return the data returned from the server', async () => {
+  const api = useApi();
+  mockedAxios.get.mockResolvedValueOnce({ data: { message: 'pong' } });
+  const response = await api.ping();
+  expect(response.message).toBe('pong');
+});
+it('should not return data when the server returns an error code', async () => {
+  const api = useApi();
+  mockedAxios.get.mockResolvedValueOnce({ data: { message: 'pong' }, status: 500 });
+  const response = await api.ping();
+  expect(response.succeeded).toBe(false);
+  expect(response.error).toBe('Failed to ping server');
+  expect(response.message).toBeNull();
+});
